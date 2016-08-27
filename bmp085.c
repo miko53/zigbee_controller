5bda5ae1c6f0ded20e95462ae9a71ab538194b8a
Mickael Sergent <mickael.sergent@free.fr>
2016-08-27 19:57:50 +0200

    add some check on read/write i2c pressure sensor

--- bmp085.c
+++ bmp085.c
@@ -13,7 +13,7 @@
 #include "daemonize.h"
 #include "gpio.h"
 #include <string.h>
-
+#include <errno.h>
 
 #define CMD_LINE_SIZE         (1024)
 #define MAIN_LOOP_WAIT_TIME   (60*5) //in s
@@ -114,98 +114,167 @@ void mainLoop(void)
   int16_t mc;
   int16_t md;
 
+  bool bIsOk = false;
 
-  bmp085_read_eeprom(0xAA, (uint16_t*) &ac1);
-  bmp085_read_eeprom(0xAC, (uint16_t*) &ac2);
-  bmp085_read_eeprom(0xAE, (uint16_t*) &ac3);
-  bmp085_read_eeprom(0xB0, &ac4);
-  bmp085_read_eeprom(0xB2, &ac5);
-  bmp085_read_eeprom(0xB4, &ac6);
-  bmp085_read_eeprom(0xB6, (uint16_t*) &b1);
-  bmp085_read_eeprom(0xB8, (uint16_t*) &b2);
-  bmp085_read_eeprom(0xBA, (uint16_t*) &mb);
-  bmp085_read_eeprom(0xBC, (uint16_t*) &mc);
-  bmp085_read_eeprom(0xBE, (uint16_t*) &md);
+  bIsOk = bmp085_read_eeprom(0xAA, (uint16_t*) &ac1);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xAC, (uint16_t*) &ac2);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xAE, (uint16_t*) &ac3);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xB0, &ac4);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xB2, &ac5);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xB4, &ac6);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xB6, (uint16_t*) &b1);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xB8, (uint16_t*) &b2);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xBA, (uint16_t*) &mb);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xBC, (uint16_t*) &mc);
+  if (!bIsOk)
+  {
+    return;
+  }
+
+  bIsOk = bmp085_read_eeprom(0xBE, (uint16_t*) &md);
+  if (!bIsOk)
+  {
+    return;
+  }
 
   uint16_t rawTemp;
   uint32_t rawPressure;
+  bool bIsOkPressure;
 
   while (1)
   {
-    bmp085_read_rawTemperature(&rawTemp);
-    bmp085_read_rawPressure(&rawPressure);
-
-    /*
-    ac1 = 408;
-    ac2 = -72;
-    ac3 = -14383;
-    ac4 = 32741;
-    ac5 = 32757;
-    ac6 = 23153;
-    b1 = 6190;
-    b2 = 4;
-    mb = -32768;
-    mc = -8711;
-    md = 2868;
-
-    rawTemp = 27898;
-    rawPressure = 23843;
-    should find temperature = 150 and pressure = 69964
-    */
-
-    int32_t x1, x2;
-    int32_t b5;
-
-    x1 = (((int32_t)rawTemp - (int32_t)ac6) * (int32_t)ac5) >> 15;
-    x2 = ((int32_t)mc << 11) / (x1 + md);
-    b5 = x1 + x2;
-
-    //   fprintf(stdout, "x1 = %d\n", x1);
-    //   fprintf(stdout, "x2 = %d\n", x2);
-    //   fprintf(stdout, "b 5= %d\n", b5);
-
-    int32_t temperature = (b5 + 8) >> 4;
-
-    int32_t b3;
-    uint32_t b4;
-    int32_t b6;
-    uint32_t b7;
-    int32_t x3;
-    int32_t pressure;
-
-    b6 = b5 - 4000;
-    x1 = (b2 * (b6 * b6) >> 12) >> 11;
-    x2 = (ac2 * b6) >> 11;
-    x3 = x1 + x2;
-    b3 = (((((long)ac1) * 4 + x3) << OSS) + 2) >> 2;
-
-    // Calculate B4
-    x1 = (ac3 * b6) >> 13;
-    x2 = (b1 * ((b6 * b6) >> 12)) >> 16;
-    x3 = ((x1 + x2) + 2) >> 2;
-    b4 = (ac4 * (unsigned long)(x3 + 32768)) >> 15;
-
-    b7 = ((unsigned long)(rawPressure - b3) * (50000 >> OSS));
-    if (b7 < 0x80000000)
+    bIsOk = bmp085_read_rawTemperature(&rawTemp);
+    if (!bIsOk)
     {
-      pressure = (b7 << 1) / b4;
-    }
-    else
-    {
-      pressure = (b7 / b4) << 1;
+      syslog(LOG_INFO, "Unable to read temperature");
     }
 
-    x1 = (pressure >> 8) * (pressure >> 8);
-    x1 = (x1 * 3038) >> 16;
-    x2 = (-7357 * pressure) >> 16;
-    pressure += (x1 + x2 + 3791) >> 4;
+    bIsOkPressure = bmp085_read_rawPressure(&rawPressure);
+    if (!bIsOkPressure)
+    {
+      syslog(LOG_INFO, "Unable to read pressure");
+    }
 
-    //fprintf(stdout, "raw pressure = %d\n", pressure);
 
-    double a = 1 - (config_altitude / 44330.);
-    double pressureAtSeaLevel = pressure / pow(a, 5.255);
+    if ((bIsOk == true) && (bIsOkPressure == true))
+    {
+      /*
+      ac1 = 408;
+      ac2 = -72;
+      ac3 = -14383;
+      ac4 = 32741;
+      ac5 = 32757;
+      ac6 = 23153;
+      b1 = 6190;
+      b2 = 4;
+      mb = -32768;
+      mc = -8711;
+      md = 2868;
 
-    build_commandline(pressureAtSeaLevel, temperature);
+      rawTemp = 27898;
+      rawPressure = 23843;
+      should find temperature = 150 and pressure = 69964
+      */
+
+      int32_t x1, x2;
+      int32_t b5;
+
+      x1 = (((int32_t)rawTemp - (int32_t)ac6) * (int32_t)ac5) >> 15;
+      x2 = ((int32_t)mc << 11) / (x1 + md);
+      b5 = x1 + x2;
+
+      //   fprintf(stdout, "x1 = %d\n", x1);
+      //   fprintf(stdout, "x2 = %d\n", x2);
+      //   fprintf(stdout, "b 5= %d\n", b5);
+
+      int32_t temperature = (b5 + 8) >> 4;
+
+      int32_t b3;
+      uint32_t b4;
+      int32_t b6;
+      uint32_t b7;
+      int32_t x3;
+      int32_t pressure;
+
+      b6 = b5 - 4000;
+      x1 = (b2 * (b6 * b6) >> 12) >> 11;
+      x2 = (ac2 * b6) >> 11;
+      x3 = x1 + x2;
+      b3 = (((((long)ac1) * 4 + x3) << OSS) + 2) >> 2;
+
+      // Calculate B4
+      x1 = (ac3 * b6) >> 13;
+      x2 = (b1 * ((b6 * b6) >> 12)) >> 16;
+      x3 = ((x1 + x2) + 2) >> 2;
+      b4 = (ac4 * (unsigned long)(x3 + 32768)) >> 15;
+
+      b7 = ((unsigned long)(rawPressure - b3) * (50000 >> OSS));
+      if (b7 < 0x80000000)
+      {
+        pressure = (b7 << 1) / b4;
+      }
+      else
+      {
+        pressure = (b7 / b4) << 1;
+      }
+
+      x1 = (pressure >> 8) * (pressure >> 8);
+      x1 = (x1 * 3038) >> 16;
+      x2 = (-7357 * pressure) >> 16;
+      pressure += (x1 + x2 + 3791) >> 4;
+
+      //fprintf(stdout, "raw pressure = %d\n", pressure);
+
+      double a = 1 - (config_altitude / 44330.);
+      double pressureAtSeaLevel = pressure / pow(a, 5.255);
+
+      build_commandline(pressureAtSeaLevel, temperature);
+    }
 
     struct timespec waitTime;
     waitTime.tv_sec = MAIN_LOOP_WAIT_TIME;
@@ -231,7 +300,7 @@ void build_commandline(double pressureAtSeaLevel, int32_t temperature)
            pressureAtSeaLevel / 100
           );
   syslog(LOG_DEBUG, "commandline: %s", commandline);
-//   fprintf(stdout, "%s\n", commandline);
+  //   fprintf(stdout, "%s\n", commandline);
   system(commandline);
 }
 
@@ -240,6 +309,7 @@ void build_commandline(double pressureAtSeaLevel, int32_t temperature)
 static bool bmp085_read_eeprom(uint8_t address, uint16_t* value)
 {
   int fd;
+  ssize_t r;
 
   fd = open(I2C_DEVICE, O_WRONLY);
   if (fd == -1)
@@ -250,7 +320,13 @@ static bool bmp085_read_eeprom(uint8_t address, uint16_t* value)
   else
   {
     ioctl(fd, I2C_SLAVE, I2C_SLAVE_ADDR);
-    write(fd, &address, 1);
+    r = write(fd, &address, 1);
+    if (r != 1)
+    {
+      syslog(LOG_ERR, "Unable to write address on the device, errno = %d", errno);
+      close(fd);
+      return false;
+    }
     close(fd);
   }
 
@@ -265,7 +341,13 @@ static bool bmp085_read_eeprom(uint8_t address, uint16_t* value)
   {
     ioctl(fd, I2C_SLAVE, I2C_SLAVE_ADDR);
     uint8_t buffer[2];
-    read(fd, buffer, 2);
+    r = read(fd, buffer, 2);
+    if (r != 2)
+    {
+      syslog(LOG_ERR, "Unable to read value on the device, errno = %d", errno);
+      close(fd);
+      return false;
+    }
     *value = ((uint16_t) buffer[0]) << 8 | buffer[1];
     //fprintf(stdout, " at : 0x%x: 0x%x 0x%x (0x%x)\n", address, buffer[0], buffer[1], *value);
     close(fd);
@@ -278,6 +360,9 @@ static bool bmp085_read_eeprom(uint8_t address, uint16_t* value)
 static bool bmp085_read_rawTemperature(uint16_t* value)
 {
   int fd;
+  bool r;
+  ssize_t s;
+
   fd = open(I2C_DEVICE, O_WRONLY);
   if (fd == -1)
   {
@@ -292,7 +377,13 @@ static bool bmp085_read_rawTemperature(uint16_t* value)
     buffer[0] = 0xF4;
     buffer[1] = 0x2E;
 
-    write(fd, buffer, 2);
+    s = write(fd, buffer, 2);
+    if (s != 2)
+    {
+      syslog(LOG_ERR, "uname to write on device to read temperature");
+      close(fd);
+      return false;
+    }
     close(fd);
   }
 
@@ -302,15 +393,17 @@ static bool bmp085_read_rawTemperature(uint16_t* value)
   nanosleep(&waitTime, NULL);
 
   //now read the result
-  bmp085_read_eeprom(0xF6, value);
-
-  return true;
+  r = bmp085_read_eeprom(0xF6, value);
+  return r;
 }
 
 
 static bool bmp085_read_rawPressure(uint32_t* value)
 {
   int fd;
+  bool r;
+  ssize_t s;
+
   fd = open(I2C_DEVICE, O_WRONLY);
   if (fd == -1)
   {
@@ -325,7 +418,13 @@ static bool bmp085_read_rawPressure(uint32_t* value)
     buffer[0] = 0xF4;
     buffer[1] = 0x34 + (OSS << 6);
 
-    write(fd, buffer, 2);
+    s = write(fd, buffer, 2);
+    if (s != 2)
+    {
+      syslog(LOG_ERR, "uname to write on device to read pressure");
+      close(fd);
+      return false;
+    }
     close(fd);
   }
 
@@ -335,9 +434,8 @@ static bool bmp085_read_rawPressure(uint32_t* value)
   nanosleep(&waitTime, NULL);
 
   //now read the result
-  bmp085_read_pressure(value);
-
-  return true;
+  r = bmp085_read_pressure(value);
+  return r;
 }
 
 
@@ -345,6 +443,7 @@ static bool bmp085_read_rawPressure(uint32_t* value)
 static bool bmp085_read_pressure(uint32_t* value)
 {
   int fd;
+  ssize_t s;
   uint8_t address;
   address = 0xF6;
 
@@ -357,7 +456,13 @@ static bool bmp085_read_pressure(uint32_t* value)
   else
   {
     ioctl(fd, I2C_SLAVE, I2C_SLAVE_ADDR);
-    write(fd, &address, 1);
+    s = write(fd, &address, 1);
+    if (s != 1)
+    {
+      syslog(LOG_ERR, "uname to write on device to read pressure");
+      close(fd);
+      return false;
+    }
     close(fd);
   }
 
@@ -372,7 +477,13 @@ static bool bmp085_read_pressure(uint32_t* value)
   {
     ioctl(fd, I2C_SLAVE, I2C_SLAVE_ADDR);
     uint8_t buffer[3];
-    read(fd, buffer, 3);
+    s = read(fd, buffer, 3);
+    if (s != 3)
+    {
+      syslog(LOG_ERR, "uname to read pressure");
+      close(fd);
+      return false;
+    }
     *value = ((uint32_t) buffer[0]) << 16 | ((uint32_t) buffer[1]) << 8 | buffer[0];
     *value = *value >> (8 - OSS);
     close(fd);
